36dc7922ab832fbe38100e18318bc41f6d60d87d
Lee <48121658+lee2nd@users.noreply.github.com>
2023-10-18 17:54:20 +0800

    Delete @ Lab 8.3 Calculator example using mocker.spy/problem directory

--- @ Lab 8.3 Calculator example using mocker.spy/problem/calculator.py
+++
@@ -1,9 +0,0 @@
-# calculator.py
-
-class Calc:
-    def add(self, a, b):
-        return a + b
-
-    def calculate_and_add(self, a, b, c):
-        result = a * b
-        return self.add(result, c)
